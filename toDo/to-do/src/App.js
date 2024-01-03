a56e690c659f75fea2ad65faa25f081e79713b07
Nayara Demarchi <nnn.demarchi@gmail.com>
2024-01-03 19:40:12 -0300

    base do projeto

--- toDo/to-do/src/App.js
+++ toDo/to-do/src/App.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+import "./App.css"
+
+import Todo from './components/Todo'
+import TodoForm from "./components/TodoForm";
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -21,22 +26,38 @@ function App() {
       isCompleted: false,
     }
   ])
+
+  const addTodo = (text, category) => {
+
+    const newTodos = [
+      ...todos, 
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+  };
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">
-                {todo.category}
-              </p>
-            </div>
-          </div>
-    
+          <Todo key={todo.id} todo={todo}/>    
         ))}
       </div>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
